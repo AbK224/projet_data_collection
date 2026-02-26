6e7cd150c1d77b9270b3dd25c6687b9f72a97152
AbK224 <koneabou669@gmail.com>
2026-02-26 13:36:01 +0000

    Add Chrome options and service imports for Selenium

--- scrapers/url1_chiens.py
+++ scrapers/url1_chiens.py
@@ -1,4 +1,7 @@
 from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
 from bs4 import BeautifulSoup as bs
 import pandas as pd
 
